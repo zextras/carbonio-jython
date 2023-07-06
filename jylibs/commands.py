c3d271478341995a8ba951678b4ba1e8e7f342f6
Davide Frison <davide.frison@zextras.com>
2023-07-06 15:20:38 +0200

    chore: conf -> config module

--- jylibs/commands.py
+++ jylibs/commands.py
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 from logmsg import *
-from org.apache.logging.log4j.core.conf import Configurator
+from org.apache.logging.log4j.core.config import Configurator
 import shlex
 import subprocess
 import time
